7119cf557e01f78bb68131fb1c5121e2b2535adc
Valeenfe <valeefehler@gmail.com>
2025-05-19 13:34:08 -0300

    Delete 51972 directory

--- 51972/ssl-antlr-dowhile/CustomCalculatorListener.js
+++
@@ -1,9 +0,0 @@
-import DowhileListener from "./generated/DoWhileListener.js";
-
-export class CustomDoWhileListener extends DoWhileListener {
-
-    enterStat(ctx) {
-        console.log(`Se detectó una: ${ctx.constructor.name}`);
-    }
-
-}
--- 51972/ssl-antlr-dowhile/CustomDoWhileVisitor.js
+++
@@ -1,5 +0,0 @@
-import DoWhileVisitor from "./generated/DoWhileVisitor.js";
-
-export class CustomDoWhileVisitor extends DoWhileVisitor {
-    // Aquí puedes agregar métodos personalizados si lo necesitas
-}
